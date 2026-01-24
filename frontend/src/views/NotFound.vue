4088848ebe23ca46c5060a0ff18b7257f5745fa3
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-01-24 04:52:36 -0300

    Mudado o notFound

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,7 +1,12 @@
 <template>
   <v-container class="text-center mt-12">
     <h1 class="text-h2 font-weight-bold">404</h1>
-    <p>Página não encontrada</p>
-    <v-btn color="primary" @click="$router.push('/')">Voltar para Home</v-btn>
+    <p>Página não encontrada ou usuário sem permissão</p>
+    <v-btn color="primary" @click="$router.push('/')">Voltar para a página inicial</v-btn>
   </v-container>
 </template>
+
+
+<script>
+import { useRouter} from 'vue-router'
+</script>
